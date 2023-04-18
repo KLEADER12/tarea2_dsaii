16d33a985b20a7d6443c07ab2e9d348e5a756bf8
kleader12 <klider402@gmail.com>
2023-04-18 01:07:01 -0600

    concatenacion sin display

---
+++ lib/homepage.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final _text1Controller = TextEditingController();
+  final _text2Controller = TextEditingController();
+
+  String _concatenatedText = '';
+
+  void _handleConcatenate() {
+    String text1 = _text1Controller.text;
+    String text2 = _text2Controller.text;
+    setState(() {
+      _concatenatedText = '$text1 $text2';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _text1Controller,
+              decoration: InputDecoration(
+                hintText: 'inserte texto 1',
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    _text1Controller.clear();
+                  },
+                  icon: const Icon(Icons.clear),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _text2Controller,
+              decoration: InputDecoration(
+                hintText: 'inserte texto 2',
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    _text2Controller.clear();
+                  },
+                  icon: const Icon(Icons.clear),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            MaterialButton(
+              onPressed: _handleConcatenate,
+              color: Colors.black87,
+              child: const Text('Concatenar', style: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 16.0),
+            Text(_concatenatedText),
+          ],
+        ),
+      ),
+    );
+  }
+}
